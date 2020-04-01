93326a5824c3b694287aa349a7fdc9291f554323
xiaobin88 <283817727@qq.com>
2020-04-01 14:08:14 +0800

    新增JS登录校验 欢迎页面

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <title>后台登录 - {{ config('app.name', 'Laravel') }}</title>
+    <title>后台登录 - {{ config('app.name', '易学教育') }}</title>
     <meta name="keywords" content="后台登录">
     <meta name="description" content="后台登录">
     <link href="{{asset('/admin/css/bootstrap.min.css')}}" rel="stylesheet">
@@ -25,22 +25,20 @@
 
         </div>
         <div class="col-sm-5 animated fadeInRight">
-            {{--<form method="post" action="{{ redirect()->action('Admin\AdminsController@loginHandle') }}">--}}
             <form id="addform" name="addform">
                 {{csrf_field()}}
                 <p class="login-title">登录</p>
-                <p class="m-t-md" style="color:#666">登录到{{ config('app.name', 'Laravel') }}系统后台管理</p>
+                <p class="m-t-md" style="color:#666">登录到{{ config('app.name', '易学教育') }}系统后台管理</p>
                 <input type="text" class="form-control uname" name="login_name" value="{{old('name')}}" required placeholder="用户名" />
                 <input type="password" class="form-control pword m-b" name="pwd" required placeholder="密码" />
                 <p></p>
                 <button class="btn btn-success btn-block" id="loginButton" name="loginButton">登录</button>
-
-                <a href="">注册</a>
+{{--                <a href="">注册</a>--}}
             </form>
         </div>
     </div>
     <div class="signup-footer animated fadeInUp">
-        &copy; 2018 All Rights Reserved.Hello world
+        &copy; {{ config('app.name', '易学教育') }}
     </div>
 </div>
 <script src="{{asset('/admin/js/jquery.min.js?v=2.1.4')}}"></script>
@@ -50,6 +48,15 @@
 <script type="application/javascript">
     $(function () {
         $('#loginButton').on('click', function () {
+            if ($.trim($("#login_name").val()) == "") {
+                sweetAlert("登录失败","用户名不能为空",'error');
+                return false;
+            }
+
+            if ($.trim($("#pwd").val()) == "") {
+                sweetAlert("登录失败","密码不能为空",'error');
+                return false;
+            }
             $.ajax({
                 type: "POST",
                 dataType: "json",
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', '易学教育') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -65,34 +65,21 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ config('app.name', '易学教育') }}
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/">首页</a>
+                    <a href="https://laracasts.com">关于魔数</a>
+                    <a href="https://laravel-news.com">课程体系</a>
+                    <a href="https://blog.laravel.com">教研教学</a>
+                    <a href="https://nova.laravel.com">AI学习平台</a>
+                    <a href="https://forge.laravel.com">OMO模式</a>
+                    <a href="https://vapor.laravel.com">全国校区</a>
+                    <a href="https://github.com/laravel/laravel">加盟授权</a>
+                    <a href="https://github.com/laravel/laravel">APP下载</a>
                 </div>
             </div>
         </div>
